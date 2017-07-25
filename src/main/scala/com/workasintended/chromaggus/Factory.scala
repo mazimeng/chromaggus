59b379db5e06e2d695a75b25905cb71b5d4ba55a
mazm <mazm@unicc.com.cn>
2017-07-25 20:11:02 +0800

    wip

--- src/main/scala/com/workasintended/chromaggus/Factory.scala
+++ src/main/scala/com/workasintended/chromaggus/Factory.scala
@@ -3,6 +3,7 @@ package com.workasintended.chromaggus
 import com.badlogic.ashley.core.{ComponentMapper, Engine, Entity, EntityListener}
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.{Animation, TextureRegion}
+import com.badlogic.gdx.math.Vector2
 import com.badlogic.gdx.scenes.scene2d.Touchable
 import com.workasintended.chromaggus.component.{ActorComponent, SelectionComponent, TransformComponent}
 
@@ -31,6 +32,32 @@ object Factory {
     entity.add(actorComponent)
     entity.add(transformComponent)
     entity.add(selectionComponent)
+
+    entity
+  }
+
+  def makeCharacter(): Entity = {
+    val char00 = new Texture("char00.png")
+    val char01 = new Texture("char01.png")
+    val char02 = new Texture("char02.png")
+
+    val char01Frames = TextureRegion.split(char01, char01.getWidth / 3, char01.getHeight / 4)
+    val char00Frames = TextureRegion.split(char00, char00.getWidth / 12, char00.getHeight / 8)
+    val char02Frames = TextureRegion.split(char02, char00.getWidth / 12, char00.getHeight / 8)
+
+    val frames = new Array[TextureRegion](2)
+    val animation = new Animation[TextureRegion](0.5f, char01Frames(0)(0), char01Frames(0)(2))
+
+    val actor = new GameActor(animation)
+    actor.setSize(32, 32)
+    val actorComponent = new ActorComponent(actor)
+    val transformComponent = new TransformComponent(new Vector2(32, 32))
+
+    val entity = new Entity()
+    entity.add(actorComponent)
+    entity.add(transformComponent)
+
+    actor.entity = entity
 
     entity
   }
--- src/main/scala/com/workasintended/chromaggus/GameScreen.scala
+++ src/main/scala/com/workasintended/chromaggus/GameScreen.scala
@@ -1,15 +1,11 @@
 package com.workasintended.chromaggus
 
-import com.badlogic.ashley.core.{Engine, Entity}
-import com.badlogic.ashley.signals.{Listener, Signal}
+import com.badlogic.ashley.core.Engine
 import com.badlogic.gdx.ScreenAdapter
 import com.badlogic.gdx.audio.Sound
 import com.badlogic.gdx.graphics.Texture
-import com.badlogic.gdx.graphics.g2d.{Animation, TextureRegion}
-import com.badlogic.gdx.math.Vector2
 import com.badlogic.gdx.scenes.scene2d.Stage
 import com.badlogic.gdx.scenes.scene2d.ui._
-import com.workasintended.chromaggus.component.{ActorComponent, TransformComponent}
 import com.workasintended.chromaggus.system.{ControlSystem, RenderSystem, SelectionSystem}
 
 
@@ -33,7 +29,7 @@ class GameScreen extends ScreenAdapter {
     engine.addSystem(inputSystem)
     engine.addSystem(selectionHighlightSystem)
 
-    val entity = makeCharacter()
+    val entity = Factory.makeCharacter()
     engine.addEntity(entity)
   }
 
@@ -46,38 +42,6 @@ class GameScreen extends ScreenAdapter {
     Service.assetManager.load("sound/melee.wav", classOf[Sound])
     Service.assetManager.load("uiskin.json", classOf[Skin])
     Service.assetManager.finishLoading()
-  }
-
-  def makeCharacter(): Entity = {
-    val char00 = new Texture("char00.png")
-    val char01 = new Texture("char01.png")
-    val char02 = new Texture("char02.png")
-
-    val char01Frames = TextureRegion.split(char01, char01.getWidth / 3, char01.getHeight / 4)
-    val char00Frames = TextureRegion.split(char00, char00.getWidth / 12, char00.getHeight / 8)
-    val char02Frames = TextureRegion.split(char02, char00.getWidth / 12, char00.getHeight / 8)
-
-    val frames = new Array[TextureRegion](2)
-    val animation = new Animation[TextureRegion](0.5f, char01Frames(0)(0), char01Frames(0)(2))
-
-    val actor = new GameActor(animation)
-    actor.setSize(32, 32)
-    val actorComponent = new ActorComponent(actor)
-    val transformComponent = new TransformComponent(new Vector2(32, 32))
-
-    val entity = new Entity()
-    entity.add(actorComponent)
-    entity.add(transformComponent)
-
-    actor.entity = entity
-
-    entity
-  }
-
-  def makeSelection(): scala.Unit = {
-    val itemTexture: Texture = Service.assetManager.get("spritesheet/selection.png")
-    val selectionTextureRegions = TextureRegion.split(itemTexture, itemTexture.getWidth() / 2, itemTexture.getHeight / 1)
-    val selectionAnimation = new Animation[TextureRegion](0.5f, selectionTextureRegions(0)(0), selectionTextureRegions(0)(1))
   }
 
 //  def initStage(): Unit = {

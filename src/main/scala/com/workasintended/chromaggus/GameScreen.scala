8134653792eb46133af0536a905c1b089ced44c0
mazm <mazm@unicc.com.cn>
2017-07-29 19:36:35 +0800

    add fps

--- src/main/scala/com/workasintended/chromaggus/GameScreen.scala
+++ src/main/scala/com/workasintended/chromaggus/GameScreen.scala
@@ -5,7 +5,7 @@ import com.badlogic.gdx.ScreenAdapter
 import com.badlogic.gdx.audio.Sound
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.math.Vector2
-import com.badlogic.gdx.scenes.scene2d.Stage
+import com.badlogic.gdx.scenes.scene2d.{Actor, Stage}
 import com.badlogic.gdx.scenes.scene2d.ui._
 import com.workasintended.chromaggus.system._
 
@@ -13,12 +13,27 @@ class GameScreen extends ScreenAdapter {
   val engine = new Engine()
   val stage = new Stage()
   val ui = new Stage()
-  Factory.engine = engine
 
+  var frameCount = 0
+  var timeElapsed = 0f
+
+  Factory.engine = engine
   init()
+
+  val fps: Container[Label] = makeFps()
+  ui.addActor(fps)
 
   override def render(delta: Float): scala.Unit = {
     engine.update(delta)
+    timeElapsed += delta
+    frameCount += 1
+
+    if(timeElapsed >= 0.5f) {
+      fps.getActor.setText(s"fps: ${frameCount / timeElapsed}")
+      timeElapsed = 0
+      frameCount = 0
+    }
+
   }
 
   def init(): scala.Unit = {
@@ -39,6 +54,18 @@ class GameScreen extends ScreenAdapter {
 
     engine.addEntity(Factory.makeCharacter(new Vector2(0, 0)))
     engine.addEntity(Factory.makeCharacter(new Vector2(256, 256)))
+  }
+
+  def makeFps(): Container[Label] = {
+    val fps = new Container[Label]()
+    fps.setFillParent(true)
+    fps.setDebug(true, true)
+    fps.top().left()
+
+    val skin: Skin = Service.assetManager.get("uiskin.json")
+    val label = new Label("fpx: 0", skin)
+    fps.setActor(label)
+    fps
   }
 
   def initAssets(): scala.Unit = {
